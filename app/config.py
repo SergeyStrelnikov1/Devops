ccfe9139c97087cdd779d12dfbaecd5e6e522e5d
Сергей Андреевич Стрельников <90750905+SergeyStrelnikov1@users.noreply.github.com>
2024-03-04 12:22:45 +0300

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,8 +1,7 @@
 import os
 
 SECRET_KEY = '123456qwerty'
-SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://std_2059_discounts:12345678@std-mysql.ist.mospolytech.ru/std_2059_discounts'
-# SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:12345678@mysql_db/discounts'
+SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:12345678@mysql_db/discounts'
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 SQLALCHEMY_ECHO = False
 
